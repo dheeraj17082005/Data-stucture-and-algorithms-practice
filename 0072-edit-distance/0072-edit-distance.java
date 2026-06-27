0ef8d633c6680635ff355dfd91a6afbe84848ee3
dheeraj17082005 <dk468055@gmail.com>
2026-06-27 14:18:27 +0530

    Time: 5 ms (69.21%), Space: 47.2 MB (52.56%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,27 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
+    public int minDistance(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
 
-        int dp[][] = new int[n][m];
+        int dp[][] = new int[n+1][m+1];
 
-        for(int row[] : dp) Arrays.fill(row,-1);
-
-        return solve(n-1,m-1,word1,word2,dp);
-    }
-    public int solve(int i, int j, String s1, String s2, int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1.charAt(i)==s2.charAt(j)){
-        return dp[i][j] = 0 + solve(i-1,j-1,s1,s2,dp);
+        for(int j = 0; j<=m; j++){
+            dp[0][j] = j;
         }
-        else{
-            return dp[i][j] = 1 + Math.min(solve(i,j-1,s1,s2,dp),Math.min(solve(i-1,j,s1,s2,dp),solve(i-1,j-1,s1,s2,dp)));
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = i;
         }
-
+    
+        for(int i = 1;i<=n; i++){
+            for(int j = 1;j<=m; j++){
+                 if(s1.charAt(i-1)==s2.charAt(j-1)){
+                   dp[i][j] = 0 + dp[i-1][j-1];
+               }
+                else{
+                  dp[i][j] = 1 + Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+               }
+            }
+        }
+        return dp[n][m];
     }
 }
