ed89b0035fb12576b17c1e89e7b5abdb822f5a56
dheeraj17082005 <dk468055@gmail.com>
2026-07-21 17:16:14 +0530

    Time: 0 ms (100%), Space: 45.7 MB (29.23%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public void swap(int[] nums,int idx,int i){
+        int temp = nums[idx];
+        nums[idx] = nums[i];
+        nums[i] = temp;
+    }
+    public void getpermutations(int[] nums,  List<List<Integer>> ans,int idx ){
+
+        if(idx == nums.length){
+           List<Integer> current = new ArrayList<>();
+           for(int num : nums){
+            current.add(num);
+           }
+           ans.add(current);
+
+        }
+        for(int i = idx; i<nums.length;i++){
+
+            swap(nums,idx,i);
+
+            getpermutations(nums,ans,idx+1);
+
+            // backtracking
+
+            swap(nums,idx,i);
+
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+       List<List<Integer>> ans = new ArrayList<>();
+        getpermutations(nums,ans,0);
+
+        return ans;
+    }
+}
